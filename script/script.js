83e41ddb794b7b1df0b93657488135ad97fc126e
Ryan  Vitor <ryanvitor29@gmail.com>
2023-07-18 01:27:55 -0300

    atualizei o JS

---
+++ script/script.js
@@ -0,0 +1,11 @@
+
+function clickSeta() {
+   let click = document.getElementById("segui-fav");
+  
+   if (click.style.display == 'none') {
+    click.style.display = 'block';
+   }
+   else {
+    click.style.display = 'none';
+   }
+}
