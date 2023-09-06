f45386ccd60481daa8da87e4798d4377092aca07
Ryan  Vitor <ryanvitor29@gmail.com>
2023-09-06 02:35:52 -0300

    novas atualizações

--- script/script.js
+++ script/script.js
@@ -9,3 +9,54 @@ function clickSeta() {
     click.style.display = 'none';
    }
 }
+
+
+
+const listaPerfisStorys = [
+   {
+      nome: 'pedro368',
+      imagem: 'pedro.jpg'
+
+   },
+   {
+      nome: 'marcos',
+      imagem: 'marcos.jpg'
+
+   },
+   {
+      nome: 'phzera123',
+      imagem: 'ph.jpg'
+
+   },
+   {
+      nome: 'juliaa',
+      imagem: 'julia.jpg'
+
+   },
+   {
+      nome: 'aninhas2',
+      imagem: 'ana.jpg'
+
+   },
+   {
+      nome: 'lucas78',
+      imagem: 'lucas.jpg'
+
+   }
+];
+
+for (const perfisStorys of listaPerfisStorys) {
+   
+   const perfilStory  =  
+   
+   `<li>
+      <div class="storys-area">
+         <div class="fotos">
+            <img src="img/${perfisStorys.imagem}" alt="foto do perfil">
+         </div>
+      </div>
+      <p>${perfisStorys.nome}</p>
+   </li>`;
+
+   document.getElementById("ul-storys").innerHTML += perfilStory;
+}
